40da2b7872c5ceb5579a06b82d851c8240a48119
EchosSense <65138796+EchosSense@users.noreply.github.com>
2020-05-13 22:59:03 +0300

    Update Bot_Yandex_Full.js

--- Bot_Yandex/Bot_Yandex_Full.js
+++ Bot_Yandex/Bot_Yandex_Full.js
@@ -10,8 +10,13 @@
 // @grant        none
 // ==/UserScript==
 
+
+let my_site = 'xn----7sbab5aqcbiddtdj1e1g.xn--p1ai';
+
 // 1. Задаём Коллекцию ключевых слов
 let collection_words = ["Гобой", "Как звучит флейта", "Что такое валторна", "Как выглядит тромбон", "Музыкальные диктанты онлайн", "Виолончель"];
+
+
 
 // 2. Выбираем случайное слово из коллекции
 let random_word = collection_words[getRandom(0,collection_words.length)];
@@ -26,15 +31,35 @@ else { alert("ЧТО-ТО ПОШЛО НЕ ТАК"); }                           
 setTimeout(()=>{location.href = "https://www.google.com/"}, 180000);                 // После завершения работы бота переходим на сайт https://www.google.com/ 
 
 // =================================== Функции ==============================================
-function F1()     // Функция: Действия на сайте https://ya.ru
+function F1(A1)     // Функция: Действия на сайте https://ya.ru
 {   
-    return Math.floor(Math.random()*(max-min)+min);
+    document.getElementsByName('text')[0].value = random_word;                       // Вставляем в форму поиска случайное ключевое слово
+    document.getElementsByClassName('button suggest2-form__button button_theme_websearch button_size_xl i-bem')[0].click();     // Нажимаем кнопку Поиск
+
+    if (flag1==true)
+    {   
+        let ALL_SITE = document.querySelectorAll("a");                                   // Получили Коллекцию ссылок на странице https://yandex.ru
+
+        for (let SITE of ALL_SITE)
+        {
+            if(SITE.href.indexOf(my_site)!=-1)                                           // Сравниваем Наш текст my_site с элементом коллекции
+            {
+                SITE.click();
+                break;
+                flag1=false;
+            }
+        }
+     else                                                                                // Нажимаем кнопку "Дальше"
+     {
+            document.getElementsByClassName('link link_theme_none link_target_serp pager__item pager__item_kind_next i-bem')[0].click();
+     }
 }
+    
 
 // =================================== Функции ==============================================
 function F2()     // Функция: Действия на сайте https://yandex.ru
 {   
-    return Math.floor(Math.random()*(max-min)+min);
+let B2_Yandex= document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem')[0];
 }
 
 // =================================== Функции ==============================================
@@ -43,18 +68,11 @@ function F3()     // Функция: Действия на сайте https://м
     return Math.floor(Math.random()*(max-min)+min);
 }
 
-// Вставили текст "Гобой" в форму поиска
-document.getElementsByName('text')[0].value = "Гобой";
 
 
-let B1_Ya= document.getElementsByClassName('button suggest2-form__button button_theme_websearch button_size_xl i-bem')[0];
-let B2_Yandex= document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem')[0];
+indexOf
 
-let FLAG = false;
 
-if (X1!= undefined) {X1.click(); FLAG=true;}
-else if (X2!= undefined) {X2.click(); FLAG=true;}
-else {console.log("ЧТО-ТО ПОШЛО НЕ ТАК")}
 
 // Проверили поиск нужной ссылки и нажали на неё
 let MY_SITE = 'https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/musical_instruments/oboe'
@@ -64,17 +82,18 @@ for (let SITE of ALL_SITE)
 {
      if (SITE==MY_SITE) {console.log("ШАГ 3"); SITE.click(); break;}
      else {console.log("Сайт не найден")}
+    indexOf
 };
 
 
 // =================================== Функции ==============================================
-
-function getRandom(min,max)     // Функция: Возращает случайное число от "min" до "max" число
+// =================================== Функция: Возращает случайное число от "min" до "max" число
+function getRandom(min,max)
 {    
     return Math.floor(Math.random()*(max-min)+min);
 }
 
-// =================================== Функции ==============================================
+// =================================== Функция: Поиска ссылки на https://yandex.ru
 function writeWord(keyword)     // Функция: Возращает случайное число от "min" до "max" число
 {
   let i = 0;
